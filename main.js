485cb242bf4a62836e7ec440dbd1c79d9f0a74d2
Laura Ferrando <lauraferrandof@gmail.com>
2019-03-04 18:48:23 +0100

    Create function to handle keydown. Create function to play sounds

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function handleKeyDown(event) {
+  const keyCode = event.keyCode;
+  
+  const audioEl = document.querySelector(`audio[data-key="${keyCode}"]`);
+
+  playSound(audioEl);
+}
+
+function playSound(audio) {
+  if(audio) {
+    audio.load();
+    audio.play();
+  }
+}
+
+window.addEventListener('keydown', handleKeyDown);
