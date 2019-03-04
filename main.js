4544ed553040474ecc75bfffe528c7c18ccca8b6
Laura Ferrando <lauraferrandof@gmail.com>
2019-03-04 19:30:28 +0100

    Create functions to remove transition styles

--- main.js
+++ main.js
@@ -2,20 +2,42 @@
 
 function handleKeyDown(event) {
   const keyCode = event.keyCode;
-  
+
   const audioEl = document.querySelector(`audio[data-key="${keyCode}"]`);
+  const keyElement = document.querySelector(`.key[data-key="${keyCode}"]`);
 
   playSound(audioEl);
 
-  const keyElement = document.querySelector(`.key[data-key="${keyCode}"]`);
+  addTransition(keyElement);
 
-  keyElement.classList.add('playing');
+  removeTransition();
 }
 
 function playSound(audio) {
-  if(audio) {
+  if (audio) {
     audio.load();
     audio.play();
+  }
+}
+
+function addTransition(key) {
+  if (key) {
+    key.classList.add('playing');
+  }
+}
+
+function removeTransition() {
+  const keyElements = document.querySelectorAll('.key');
+
+  keyElements.forEach(keyEl => {
+    keyEl.addEventListener('transitionend', handleTransitionEnd);
+  })
+}
+
+function handleTransitionEnd(event) {
+  const propertyName = event.propertyName;
+  if (propertyName === 'transform') {
+    this.classList.remove('playing');
   }
 }
 
